eddb0c0606fb5a0ec638f8b3243502c763b2bb91
root <leovalenciasantana@gmail.com>
2020-09-17 20:33:44 +0000

    new

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Prints the method
+ *Return: Always 0
+ */
+int main(void)
+{
+char letra;
+for (letra = 'z'; letra >= 'a'; letra--)
+putchar(letra);
+
+putchar('\n');
+return (0);
+}
