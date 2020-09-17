a8062fc049c6b9ab8264ec727c01c764bc8e0cdd
root <leovalenciasantana@gmail.com>
2020-09-17 18:00:54 +0000

    new

---
+++ 0x01-variables_if_else_while/.#1-last_digit.c
@@ -0,0 +1,1 @@
+root@vagrant-ubuntu-trusty-64.1814:1600363146
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,9 +8,10 @@ int main(void)
 int a = 0;
 while (a <= 9)
 {
-printf("%d \n", a);
+printf("%d", a);
 a++;
 }
+printf("\n");
 
 return (0);
 }
