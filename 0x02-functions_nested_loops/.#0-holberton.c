eb1dc91fd5a60868da576dc4ef35299499fba3b2
root <leovalenciasantana@gmail.com>
2020-09-21 21:06:06 +0000

    Functions

--- 0x02-functions_nested_loops/.#0-holberton.c
+++
@@ -1,1 +0,0 @@
-vagrant@vagrant-ubuntu-trusty-64.1497:1600690461
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *main - Prints the method
+ *Return: Always 0
+ */
+void print_alphabet(void)
+{
+char letra;
+for (letra = 'a'; letra <= 'z'; letra++)
+_putchar(letra);
+
+_putchar('\n');
+}
