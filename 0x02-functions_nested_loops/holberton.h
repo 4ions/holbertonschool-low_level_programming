568e4b5fa21a8a3b9eef4b1cf138fa0a601bc810
root <leovalenciasantana@gmail.com>
2020-09-22 12:29:45 +0000

    loops

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -7,3 +7,43 @@ int _putchar(char c);
 #define PRINT_ALPHABET_H
 void print_alphabet(void);
 #endif
+
+#ifndef PRINT_ALPHABET_x10_H
+#define PRINT_ALPHABET_x10_H
+void print_alphabet_x10(void);
+#endif
+
+#ifndef _ISLOWER_H
+#define _ISLOWER_H
+int _islower(int c);
+#endif
+
+#ifndef _ISALPHA_H
+#define _ISALPHA_H
+int _isalpha(int c);
+#endif
+
+#ifndef PRINT_SIGN_H
+#define PRINT_SIGN_H
+int print_sign(int n);
+#endif
+
+#ifndef _ABS_H
+#define _ABS_H
+int _abs(int);
+#endif
+
+#ifndef PRINT_LAST_DIGIT_H
+#define PRINT_LAST_DIGIT_H
+int print_last_digit(int);
+#endif
+
+#ifndef JACK_BAUER_H
+#define JACK_BAUER_H
+void jack_bauer(void);
+#endif
+
+#ifndef TIMES_TABLE_H
+#define TIMES_TABLE_H
+void times_table(void);
+#endif
