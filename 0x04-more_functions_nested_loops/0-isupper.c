4c4b4d0d6bb3d0c5ca75bda56f4d31d069ec3592
root <leovalenciasantana@gmail.com>
2020-09-24 15:07:22 +0000

    More Loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _isupper - To identify is a letter is uppercase or  not
+ * Return: 1 if is a uppercase
+ * @c: be the number in the loop
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+
+else
+return (0)
+}
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,9 @@
+#ifndef _PUTCHAR_H
+#define _PUTCHAR_H
+int _putchar(char c);
+#endif
+
+#ifndef _ISUPPER_H
+#define _ISUPPER_H
+int _isupper(int c);
+#endif
