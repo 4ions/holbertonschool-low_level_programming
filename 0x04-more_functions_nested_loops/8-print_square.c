67a7c8e5db2f5aab77b06944bcea3367288a31c1
root <leovalenciasantana@gmail.com>
2020-09-24 18:00:37 +0000

    More Loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *print_square - function print a square
+ *@size: size of square
+ *Return: No return
+ */
+void print_square(int size)
+{
+int i, j;
+if (size > 0)
+{
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
+}
