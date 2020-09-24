5f85bc52d92d66e7cca2cd5f3b1e1289b94526a0
root <leovalenciasantana@gmail.com>
2020-09-24 21:25:54 +0000

    More Loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - function to print the mayor number
+ *Return: no return
+ */
+
+int main(void)
+{
+long int i, numero;
+numero = 612852475143;
+for (i = 2 ; i < numero ; i++)
+{
+if (numero % i == 0)
+{
+while (numero % i == 0)
+numero /= i;
+}
+}
+printf("%li\n ", numero);
+return (0);
+}
