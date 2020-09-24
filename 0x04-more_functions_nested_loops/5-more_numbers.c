5a885e5aab6007d40587a32fd0b6992eba8d3d1c
root <leovalenciasantana@gmail.com>
2020-09-24 17:19:52 +0000

    More Loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,15 @@
 void more_numbers(void)
 {
 int i, j;
-for (j = 0; j <= 10; j++)
+for (i = 0; i <= 10; i++)
 {
-for (i = 0; i <= 14; i++)
+for (j = 0; j <= 14; j++)
 {
-if (i / 10 != 0)
+if (j / 10 != 0)
 {
-_putchar('0' + i / 10);
+_putchar('0' + j / 10);
 }
-_putchar('0' + i % 10);
+_putchar('0' + j % 10);
 
 }
 _putchar('\n');
