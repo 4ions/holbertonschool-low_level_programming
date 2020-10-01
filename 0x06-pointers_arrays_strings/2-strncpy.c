1f8d5f69bce10f970b1f72818967f7eda94ff01a
root <leovalenciasantana@gmail.com>
2020-10-01 19:52:33 +0000

    Pointers and Arrays

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+B#include "holberton.h"
+/**
+ *_strncpy - Copy a string
+ *@dest: destination char
+ *@src: source
+ *@n: n of lines
+ *
+ *Return: char value
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+
+for ( ; i < n; i++)
+dest[i] = '\0';
+
+return (dest);
+}
