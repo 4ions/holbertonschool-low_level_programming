823625b67fae1904edcec26468c2ce7020c0b929
root <leovalenciasantana@gmail.com>
2020-10-01 22:44:14 +0000

    Pointers And Arrays

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ *reverse_array - Reverses the cointent of an array
+ *@a: pointer
+ *@n: numbers of elments of the array
+ *
+ *Return: no return
+ */
+void reverse_array(int *a, int n)
+{
+	int j, l;
+	int tmp;
+
+	j = 0;
+	l = n / 2;
+	n = n - 1;
+
+	while (n  >= l)
+	{
+		tmp = a[j];
+		a[j] = a[n];
+		a[n] = tmp;
+		n--;
+		j++;
+	}
+
+}
