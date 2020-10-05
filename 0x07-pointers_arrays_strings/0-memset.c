53abfa0fbf7738486eb3bf879e8c08471414c34c
root <leovalenciasantana@gmail.com>
2020-10-05 19:26:50 +0000

    More Pointers and Arrays

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *_memset - function that fills memoty with a constant byte
+ *@s: pointer of byte
+ *@b: constant
+ *@n: bytes of the memory
+ *
+ *Return: Return char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i <= n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+
+}
---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,4 @@
+#ifndef _MENSET_H
+#define _MENSET_H
+char *_memset(char *s, char b, unsigned int n);
+#endif
