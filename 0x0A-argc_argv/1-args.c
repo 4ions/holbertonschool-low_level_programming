09f4568daca2710f06db5f20a3056f22f29f241c
root <leovalenciasantana@gmail.com>
2020-10-11 18:29:12 +0000

    argc and argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - function main
+ *@argc: argument count
+ *@argv: argument vector
+ *
+ *Return: 0 to sucess
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	sum = 0;
+	if (argc > 1)
+	{
+	for (i = 1; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+	}
+	}
+	printf("%d\n", argc);
+	return (0);
+}
