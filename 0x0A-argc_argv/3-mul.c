ef274f0ffc20b43eec2c931713f1883d86c3843d
root <leovalenciasantana@gmail.com>
2020-10-11 18:48:39 +0000

    argc and argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - function main
+ *@argc: argument count
+ *@argv: argument vector
+ *
+ *Return: 0 to sucess
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul;
+
+	mul = 1;
+	if (argc >= 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+
+		}
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
