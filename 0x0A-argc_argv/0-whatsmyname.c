68ec5c4b83bbaf96d73f214aea054508920191f2
root <leovalenciasantana@gmail.com>
2020-10-11 17:47:23 +0000

    argc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,15 @@
 #include "holberton.h"
 #include <stdio.h>
-
-int main(int argc, char* argv[])
+/**
+ *main - function main
+ *@argc: argument count
+ *@argv: argument vector
+ *
+ *Return: 0 to sucess
+ */
+int main(int argc, char *argv[])
 {
-  argc = 0;
-  printf("%s\n", argv[argc]);
-	 return 0;
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
 }
