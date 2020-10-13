46b5b6a0b2485c6068a66afd0740a6cffa0520c7
root <leovalenciasantana@gmail.com>
2020-10-13 18:14:19 +0000

    Mallow

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,21 +6,22 @@
  *@size: size of array
  *@c: character
  *
+ *Return: Char
  */
 char *create_array(unsigned int size, char c)
 {
-	unsigned int i;/**Creada para verificar el tamaño y moverse en size, tiene el mismo tipo*/
+	unsigned int i;/**verificar el tamaño y moverse en size,tiene el mismo tipo*/
 	char *s; /** puntero donde se guardaran los datos*/
 
 	if (size <= 0)
 		return (0);/** Return NULL si es menor que 0*/
 
-	s = malloc(sizeof(char) * size); /** Asignar todos los char entrantes en el puntero creado*/
+	s = malloc(sizeof(char) * size); /**Asignar todos los char al puntero creado*/
 
 	if (s == 0)
 		return (0);/** lo mismo que el otro if*/
 
-	for (i = 0; i < size; i++)/** Recorrer el array y asignar cada caracter c introducido*/
+	for (i = 0; i < size; i++)/**asignar cada caracter introducido*/
 		*(s + i) = c;
 
 	*(s + i) = '\0';/**En la ultima posicion del for asignar el nulo*/
