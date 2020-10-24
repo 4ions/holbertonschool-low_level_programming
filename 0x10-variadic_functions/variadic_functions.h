06dc761fd1b47653b1051fe7d00d5f636e216948
root <leovalenciasantana@gmail.com>
2020-10-24 18:08:45 +0000

    Print all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,5 @@
-#ifndef _VARIADIC_FUNCTIONS_H
-#define _VARIADIC_FUNCTIONS_H
-
+#ifndef STRUCT_H
+#define STRUCT_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -16,10 +15,12 @@ typedef struct fp
 	char *tp;
 	void (*func)(va_list);
 } fp_;
+#endif
 
+#ifndef _VARIADIC_FUNCTIONS_H
+#define _VARIADIC_FUNCTIONS_H
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
 #endif
