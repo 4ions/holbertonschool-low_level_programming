d78aff013de5098012ea906140bfa34165c64de4
4ions <leovalenciasantana@gmail.com>
2020-12-12 15:19:20 +0000

    Add more functions

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -18,5 +23,7 @@ typedef struct dlistint_s
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 
 #endif /* LISTS_H */
