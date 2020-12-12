a654780969b8bf8e03efea2ce3aee26f18bc9977
4ions <leovalenciasantana@gmail.com>
2020-12-12 18:07:51 +0000

    Fixed

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,11 +3,12 @@
 #include "lists.h"
 
 /**
+ *insert_dnodeint_at_index - insert a node at a index
+ *@h: head of dobly linked list
+ *@idx: position of the new node
+ *@n: numeric value
  *
- *
- *
- *
- *
+ *Return: new node
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
