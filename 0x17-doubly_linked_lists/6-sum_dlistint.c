2ab4b43646c3144045240d43cad43cfcea189ff4
4ions <leovalenciasantana@gmail.com>
2020-12-14 00:50:22 +0000

    Eliminate conditionals

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -13,13 +13,8 @@ int sum_dlistint(dlistint_t *head)
 {
 	unsigned int sum;
 
-	if (!head)
-		return (0);
 	while (head)
 	{
-		if (!head)
-			return (sum);
-
 		sum += head->n;
 		head = head->next;
 	}
