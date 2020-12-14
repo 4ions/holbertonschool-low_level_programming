fa91fa97258e926c0940cbc10fa0e9bbc470f58c
4ions <leovalenciasantana@gmail.com>
2020-12-14 00:45:48 +0000

    Fixed another error of prev

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,6 +15,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 
+	if (!head)
+		return (NULL);
+
 	new_node = malloc(sizeof(dlistint_t));
 
 	if (!new_node)
@@ -23,10 +26,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 
 	new_node->n = n;
-	
+
+	new_node->prev = NULL;
 	new_node->next = *head;
 	*head = new_node;
-	new_node->prev = *head;
+
 	return (new_node);
 
 }
