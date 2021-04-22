7db91ccfa206d7dce54c738c8747ca3b2b4ac3c2
4ions <leovalenciasantana@gmail.com>
2021-04-22 08:29:58 -0500

    Binary

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,47 +1,55 @@
-#include <stdio.h>
-#include <math.h>
 #include "search_algos.h"
-/**
- *binary_search - searches for a value using the Binary search algorithm
- *@array: is a pointer to the first element of the array to search in
- *@size: is the number of elements in array
- *@value:  is the value to search for
- *
- *Return: if value is not present in array or if array is NULL, return -1
- *
- */
 
+/**
+ * print_array - prints an array of integers
+ * @array: array to print
+ * @size: size of array
+ *
+ * return: void
+ */
+void print_array(int *array, size_t size)
+{
+	size_t i;
+
+	printf("Searching in array:");
+	for (i = 0; i < size; i++)
+	{
+		printf(" %d", array[i]);
+		if (i != size - 1)
+			printf(",");
+	}
+	printf("\n");
+}
+
+/**
+ * binary_search - searches for a value in a sorted array of integers using the
+ * Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: index where value is located, or -1 on failure
+ */
 int binary_search(int *array, size_t size, int value)
 {
-	int m, i = 0, index = 0;
-	int _size = (int)size - 1;
+	size_t l, m, r;
 
-	if (array == NULL)
-		return (-1);
-
-	while (index <= _size)
+	if (array != NULL && size > 0)
 	{
-		i = index;
-		printf("Searching in array:  ");
-		while (i <= ((int)_size))
+		l = 0;
+		r = size - 1;
+		print_array(array + l, r + 1 - l);
+		while (l < r)
 		{
-			if (i == ((int)_size))
-				printf("%d", array[i]);
+			m = (l + r) / 2;
+			if (array[m] < value)
+				l = m + 1;
+			else if (array[m] > value)
+				r = m;
 			else
-				printf("%d, ", array[i]);
-			i++;
+				return (m);
+			print_array(array + l, r + 1 - l);
 		}
-		printf("\n");
-
-		m = floor((index + _size) / 2);
-
-		if (array[m] < value)
-			index = m + 1;
-		else if (array[m] > value)
-			_size = m - 1;
-		else
-			return (m);
 	}
-
 	return (-1);
 }
